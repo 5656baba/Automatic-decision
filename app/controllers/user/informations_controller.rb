be47ffb63b636fa2a025eb7dcea7f9c24015990e
EC2 Default User <ec2-user@ip-172-31-93-65.ec2.internal>
2022-03-05 07:33:58 +0000

    second commit

---
+++ app/controllers/user/informations_controller.rb
@@ -0,0 +1,43 @@
+class User::InformationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:show, :edit, :update, :withdraw, :unsubscribe]
+  
+  def show
+    @user=current_user
+  end
+
+  def edit
+    @user=current_user
+  end
+
+  def unsubscribe
+  end
+
+  def update
+    @user = current_user  #今回はcustomer_id飛ばしてないから最初からcustomer=current_customerを入れておく
+    if @user.update(user_params)
+      redirect_to customers_path
+    else
+      render :edit
+    end
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_active: false)
+    reset_session
+    flash[:notice] = "退会手続きが完了いたしました。ご利用いただき誠にありがとうございました。"
+    redirect_to root_path
+  end
+  
+  def ensure_correct_user
+    unless user_id=current_user.id
+    redirect_to root_path(current_user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+  scope module: :user do
+    resources :informations, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+    root to: 'homes#top'
+  end
+  devise_for :users
 end
