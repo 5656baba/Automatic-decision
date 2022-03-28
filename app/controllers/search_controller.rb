830a47c75ae38edc8339f63286406894a1758701
5656baba <syounenjampsan@gmail.com>
2022-03-28 19:25:14 +0000

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,12 +15,12 @@ class SearchController < ApplicationController
       recipe_ingredients = RecipeIngredient.where("ingredient LIKE ?", "%" + @keywords + "%")
     end
 
+
     recipes = []
-    recipe_ingredients.each do |recipe_ingredient|
+    recipe_ingredients.select(:id).distinct!.each do |recipe_ingredient|
       recipes.push(recipe_ingredient.recipe)
     end
-    recipe_lists = recipes.select(:id).distinct!
-    recipes_order = recipe_lists.sort_by! { |v| v.recipe_ingredients.count }
+    recipes_order = recipes.sort_by! { |v| v.recipe_ingredients.count }
     @recipes = Kaminari.paginate_array(recipes_order).page(params[:page]).per(15)
     @quantity = recipes.count
   end
