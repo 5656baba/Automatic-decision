c3b399f521eee58d728b260351166db46386cdf0
5656baba <syounenjampsan@gmail.com>
2022-03-28 19:14:30 +0000

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -19,8 +19,8 @@ class SearchController < ApplicationController
     recipe_ingredients.each do |recipe_ingredient|
       recipes.push(recipe_ingredient.recipe)
     end
-    recipes.select(:id).uniq!
-    recipes_order = recipes.sort_by! { |v| v.recipe_ingredients.count }
+    recipe_lists = recipes.select(:id).uniq!
+    recipes_order = recipe_lists.sort_by! { |v| v.recipe_ingredients.count }
     @recipes = Kaminari.paginate_array(recipes_order).page(params[:page]).per(15)
     @quantity = recipes.count
   end
