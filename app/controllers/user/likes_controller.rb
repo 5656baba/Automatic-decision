2068a4cb0b09563da44e01bee0e59de5a1916905
5656baba <syounenjampsan@gmail.com>
2022-03-30 10:38:56 +0000

    likes

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -4,14 +4,14 @@ class User::LikesController < ApplicationController
 
   def create
     @comment = Comment.find(params[:comment_id])
-    like = @comment.likes.new(user_id: current_user.id)
+    like = @comment.likes.new(user_id: current_user.id, post_id: @comment.post.id)
     like.save!
     @post = Post.find(params[:post_id])
   end                           # find idのみ  find_by カラム指定
 
   def destroy
     @comment = Comment.find_by(id: params[:comment_id], post_id: params[:post_id])
-    like = current_user.likes.find_by(comment_id: @comment.id)
+    like = current_user.likes.find_by(comment_id: @comment.id, post_id: @comment.post.id)
     like.destroy
     @post = Post.find(params[:post_id])
   end
