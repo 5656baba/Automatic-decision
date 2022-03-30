531a89b4cb03ab1cf68fc3f1f3cd8f36a0d1c44e
5656baba <syounenjampsan@gmail.com>
2022-03-30 10:44:40 +0000

    likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :comment
 
   validates :user_id, presence: true
-  validates :comment_id, uniqueness: { scope: :user_id }, presence: true
-  validates :post_id, uniqueness: { scope: :user_id }, presence: true
+  validates :comment_id, presence: true
+  validates :post_id, presence: true
 end
